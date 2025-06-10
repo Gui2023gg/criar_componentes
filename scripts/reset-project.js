fa393ef879d3bebcb41b1b3feaa7eb8ec4eba604
Gui2023gg <128082646+Gui2023gg@users.noreply.github.com>
2025-06-10 14:29:10 -0300

    Update reset-project.js

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -10,6 +10,7 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
+
 const root = process.cwd();
 const oldDirs = ["app", "components", "hooks", "constants", "scripts"];
 const exampleDir = "app-example";
